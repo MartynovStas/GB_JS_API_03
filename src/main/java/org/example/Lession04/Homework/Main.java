77e8a2a7d5424cff128b2782c83a68b47c03fa37
Stas Martynov <maks.mart71@bk.ru>
2023-07-21 13:47:35 +0400

    DZ_Lesion04
    Signed-off-by: Stas Martynov <maks.mart71@bk.ru>

---
+++ src/main/java/org/example/Lession04/Homework/Main.java
@@ -0,0 +1,24 @@
+package org.example.Lession04.Homework;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество элементов ");
+        int n = sc.nextInt();
+        sc.close();
+        Deque<Integer> ourDeque = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            ourDeque.add(i);
+        }
+        System.out.println(ourDeque);
+
+        System.out.println("Перевернутый список");
+        while (ourDeque.size() > 0) {
+            System.out.print(ourDeque.pollLast() + " ");
+        }
+    }
+}
